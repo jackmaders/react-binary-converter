c7097d8fe11b804add990f741f11aec653dd7320
Jack Maders <jackwmaders@gmail.com>
2024-02-05 09:46:18 +0000

    add tailwind and consistent heading

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import { ChakraProvider, Container } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import BinaryConverter from "./binaryConverter/BinaryConverter";
+import "./tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ChakraProvider>
     <React.StrictMode>
-      <Container>
+      <Container className="space-y-8 pt-8">
+        <h1 className="text-2xl font-semibold">Binary Converter</h1>
         <BinaryConverter />
       </Container>
     </React.StrictMode>
-  </ChakraProvider>,
+  </ChakraProvider>
 );
