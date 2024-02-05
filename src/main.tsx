0de2bdc94fa258f7940be48ffe2c28d60813721c
Jack Maders <jackwmaders@gmail.com>
2024-02-05 09:46:12 +0000

    add prettier

--- src/main.tsx
+++ src/main.tsx
@@ -10,5 +10,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <BinaryConverter />
       </Container>
     </React.StrictMode>
-  </ChakraProvider>
+  </ChakraProvider>,
 );
