8553d9e6197bab21b6e18cb1dde4394e04bef5bc
Jack Maders <jackwmaders@gmail.com>
2024-02-05 09:46:12 +0000

    add react component

---
+++ src/binaryConverter/BinaryConverter.tsx
@@ -0,0 +1,70 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import convertBinaryToDecimal from "./convertBinaryToDecimal";
+import {
+  BINARY_REGEX,
+  ERROR_BINARY_MAX_LENGTH,
+  ERROR_INVALID_BINARY,
+} from "./constants";
+
+export default function BinaryConverter() {
+  const [inputBinary, setInputBinary] = useState("");
+  const [outputDecimal, setOutputDecimal] = useState<number | undefined>();
+  const [formError, setFormError] = useState("");
+
+  useEffect(() => {
+    if (!inputBinary) {
+      setFormError("");
+      setOutputDecimal(undefined);
+      return;
+    }
+
+    if (inputBinary.length > 8) {
+      setFormError(ERROR_BINARY_MAX_LENGTH);
+      setOutputDecimal(undefined);
+      return;
+    }
+
+    if (!BINARY_REGEX.test(inputBinary)) {
+      setFormError(ERROR_INVALID_BINARY);
+      setOutputDecimal(undefined);
+      return;
+    }
+
+    const decimal = convertBinaryToDecimal(inputBinary);
+
+    setOutputDecimal(decimal);
+  }, [inputBinary]);
+
+  return (
+    <>
+      <Card>
+        <CardBody>
+          <FormControl isRequired isInvalid={!!formError}>
+            <FormLabel>Binary</FormLabel>
+            <Input
+              value={inputBinary}
+              placeholder="Please provide binary..."
+              onChange={(event) => {
+                setInputBinary(event.target.value);
+              }}
+            />
+            <FormErrorMessage>{formError}</FormErrorMessage>
+          </FormControl>
+        </CardBody>
+        <CardFooter>
+          <Text fontWeight="bold">Decimal: {outputDecimal}</Text>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ChakraProvider, Container } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BinaryConverter from "./binaryConverter/BinaryConverter";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ChakraProvider>
+    <React.StrictMode>
+      <Container>
+        <BinaryConverter />
+      </Container>
+    </React.StrictMode>
+  </ChakraProvider>
+);
